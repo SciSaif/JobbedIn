b01f5fe7318fb88c6eee60db862dd559c998b635
saifullah rahman <saiflll284@gmail.com>
2022-02-09 06:26:51 +0530

    added all jobs page

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-console.log("k");
 app.use("/api/employers", require("./routes/employerRoute"));
 app.use("/api/jobs", require("./routes/jobRoutes"));
 
-//Routes
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the JobbedIn" });
+  });
+}
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
