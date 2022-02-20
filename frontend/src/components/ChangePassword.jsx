96bbab3be343f1d96eee50051c09902760a18f9c
saifullah rahman <saiflll284@gmail.com>
2022-02-20 18:55:25 +0530

    still

--- frontend/src/components/ChangePassword.jsx
+++ frontend/src/components/ChangePassword.jsx
@@ -64,7 +64,7 @@ function ChangePassword({ closeDrawer, setSelectedSetting, setOpen }) {
   const btnRef = useRef();
 
   const handleFocus = (e) => {
-    btnRef.current.scrollIntoView();
+    // btnRef.current.scrollIntoView();
   };
 
   return (
